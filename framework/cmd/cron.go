0888e21f99dd0720985b453386e895fe0c7b97f1
lixiaoqing18 <3398732@qq.com>
2023-06-27 15:25:55 +0800

    cron restart

--- framework/cmd/cron.go
+++ framework/cmd/cron.go
@@ -5,8 +5,10 @@ import (
 	"os"
 	"path/filepath"
 	"syscall"
+	"time"
 
-	//"github.com/erikdubbelboer/gspt"
+	"github.com/erikdubbelboer/gspt"
+	"github.com/sevlyar/go-daemon"
 
 	"github.com/lixiaoqing18/zima/framework"
 	"github.com/lixiaoqing18/zima/framework/contract"
@@ -34,44 +36,43 @@ var cronStartCommand = &cobra.Command{
 		// start命令有一个deamon参数，简写为d
 		settingService := framework.MustMake(contract.SettingKey).(contract.Setting)
 		cronPidFile := filepath.Join(settingService.RuntimeFolder(), "cron.pid")
-		//cronLogFile := filepath.Join(settingService.LogFolder(), "cron.log")
-		//currentFolder := settingService.BaseFolder()
+		cronLogFile := filepath.Join(settingService.LogFolder(), "cron.log")
+		currentFolder := settingService.BaseFolder()
 		if cronDeamon {
-			/*
-				// 创建一个Context
-				cntxt := &daemon.Context{
-					// 设置pid文件
-					PidFileName: cronPidFile,
-					PidFilePerm: 0664,
-					// 设置日志文件
-					LogFileName: cronLogFile,
-					LogFilePerm: 0640,
-					// 设置工作路径
-					WorkDir: currentFolder,
-					// 设置所有设置文件的mask，默认为750
-					Umask: 027,
-					// 子进程的参数，按照这个参数设置，子进程的命令为 ./zima cron start --daemon=true
-					Args: []string{"", "cron", "start", "--daemon=true"},
-				}
-				// 启动子进程，d不为空表示当前是父进程，d为空表示当前是子进程
-				d, err := cntxt.Reborn()
-				if err != nil {
-					return err
-				}
-				if d != nil {
-					// 父进程直接打印启动成功信息，不做任何操作
-					fmt.Println("cron service started, pid:", d.Pid)
-					fmt.Println("log file:", cronLogFile)
-					return nil
-				}
 
-				// 子进程执行Cron.Run
-				defer cntxt.Release()
-				fmt.Println("daemon started")
-				gspt.SetProcTitle("zima cron daemon")
-				framework.StartCron()
+			// 创建一个Context
+			cntxt := &daemon.Context{
+				// 设置pid文件
+				PidFileName: cronPidFile,
+				PidFilePerm: 0664,
+				// 设置日志文件
+				LogFileName: cronLogFile,
+				LogFilePerm: 0640,
+				// 设置工作路径
+				WorkDir: currentFolder,
+				// 设置所有设置文件的mask，默认为750
+				Umask: 027,
+				// 子进程的参数，按照这个参数设置，子进程的命令为 ./zima cron start --daemon=true
+				Args: []string{"", "cron", "start", "--daemon=true"},
+			}
+			// 启动子进程，d不为空表示当前是父进程，d为空表示当前是子进程
+			d, err := cntxt.Reborn()
+			if err != nil {
+				return err
+			}
+			if d != nil {
+				// 父进程直接打印启动成功信息，不做任何操作
+				fmt.Println("cron service started, pid:", d.Pid)
+				fmt.Println("log file:", cronLogFile)
+				return nil
+			}
 
-			*/
+			// 子进程执行Cron.Run
+			defer cntxt.Release()
+			fmt.Println("daemon started")
+			gspt.SetProcTitle("zima cron daemon")
+			framework.StartCron()
+
 			return nil
 		} else {
 			pid := cast.ToString(os.Getpid())
@@ -79,7 +80,7 @@ var cronStartCommand = &cobra.Command{
 			if err != nil {
 				return err
 			}
-			//gspt.SetProcTitle("zima cron")
+			gspt.SetProcTitle("zima cron")
 			fmt.Println("cron service started!pid=", pid)
 			framework.StartCron()
 			return nil
@@ -101,9 +102,26 @@ var cronRestartCommand = &cobra.Command{
 	Use:   "restart",
 	Short: "重启cron任务进程",
 	RunE: func(c *cobra.Command, args []string) error {
-		cronStopCommand.RunE(c, args)
+		settingService := framework.MustMake(contract.SettingKey).(contract.Setting)
+		cronPidFile := filepath.Join(settingService.RuntimeFolder(), "cron.pid")
+		content, err := os.ReadFile(cronPidFile)
+		if err != nil {
+			return err
+		}
+		if content != nil && len(content) > 0 {
+			//linux
+			pid := cast.ToInt(string(content))
+			syscall.Kill(pid, syscall.SIGTERM)
 
-		return nil
+			for i := 0; i < 10; i++ {
+				if !util.CheckProcessExist(pid) {
+					break
+				}
+				time.Sleep(1 * time.Second)
+			}
+		}
+
+		return cronStartCommand.RunE(c, args)
 	},
 }
 
